2bd60948b0c37a4d561ffa0b0cec0d4a38094782
Linhanrr <hank093011@gmail.com>
2021-07-12 00:29:48 +0800

    revised version

--- password.py
+++ password.py
@@ -1,11 +1,16 @@
 password = 'a123456'
-n = 3
+n = 3 #剩餘機會
 while n > 0:
+    n = n - 1 #放在這裡，相當於只要問一次密碼，就用掉一次機會
     attempt = input('請輸入密碼: ')
-    if attempt == 'a123456':
+    if attempt == password:
         print('登入成功')
         break
-    elif attempt != 'a123456':
-            print('密碼錯誤! 你還有', n-1, '次機會')
-            n = n - 1
+    else:
+        print('密碼錯誤!')
+        if n > 0:
+            print('你還有', n, '次機會') #這樣在最後一次的時候，就不會印出還剩幾次
+        else:
+            print('沒機會嘗試了! 要鎖帳號ㄌ!')
+            
 
